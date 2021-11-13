0aecfd32049d6634d4631637f73219743f1f1ed2
alsoreezu <rsevnnn@gmail.com>
2021-11-13 11:45:04 +0000

    updated and added some files

---
+++ tempdir/app.py
@@ -0,0 +1,15 @@
+import ipapi
+
+from flask import Flask
+from flask import request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    search = request.form.get('search')
+    ip = ipapi.location(ip=search, output='json')
+    return render_template('index.html', ip=ip)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5050)
